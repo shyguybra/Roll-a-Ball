febb6089587e5fabfec01299466b1c97f7c3648f
shyguybra <gabiramos1@gmail.com>
2015-08-23 18:53:00 -0300

    First checkin
    just another git project

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	private Rigidbody rb;
+	public GameObject reference;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 movement = reference.transform.position;
+		rb.AddForce (movement * speed);
+	}
+}
